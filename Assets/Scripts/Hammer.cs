77f1d3042707130b492f9d6010941278feda0f4e
chrisJohns0n <126682002+ChrisJohnson0110@users.noreply.github.com>
2023-05-11 16:44:08 +0100

    Dialog interact / audio
    updated dialog interaction -
    easier to use
    fixed errors
    more modular
    Also added audio file of beat and added to testing camera

--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -10,15 +10,20 @@ using UnityEngine;
 public class Hammer : MonoBehaviour, IDataPersistence
 {
     EricInteraction diaInterRef;
-    QuestDisplayManager qdmQuestRef;
+    QuestDisplayManager QuestDisplayManagerRef;
 
     public bool bHammerFound = false;
+
+    public int iQuestID; //id of the quest this script gives //new id
+    public int iHandinQuestID; //id of the quest that needs to be cleared // erics
+    public string sQuestDialog; //what the quest area dialog is going to be
 
     // Start is called before the first frame update
     void Start()
     {
         diaInterRef = FindAnyObjectByType<EricInteraction>();
-        qdmQuestRef = FindAnyObjectByType<QuestDisplayManager>();
+        QuestDisplayManagerRef = FindAnyObjectByType<QuestDisplayManager>();
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -26,7 +31,10 @@ public class Hammer : MonoBehaviour, IDataPersistence
         if (other.gameObject.tag == "Player")
         {
             bHammerFound = true;
-            diaInterRef.HammerFound();
+
+            QuestDisplayManagerRef.RemoveQuest(iHandinQuestID);
+            QuestDisplayManagerRef.AddNewQuest(iQuestID, sQuestDialog);
+
             gameObject.GetComponent<Renderer>().enabled = false;
             gameObject.GetComponent<Collider>().enabled = false;
             
